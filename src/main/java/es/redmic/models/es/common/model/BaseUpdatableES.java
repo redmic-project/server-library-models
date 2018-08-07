da8fe88128cdfb00de1f5ece4638c7234f0f1f34
Noel Alonso <noel@redmic.es>
2018-08-07 12:51:41 +0100

    Añade nueva base con campos updated e inserted

---
+++ src/main/java/es/redmic/models/es/common/model/BaseUpdatableES.java
@@ -0,0 +1,36 @@
+package es.redmic.models.es.common.model;
+
+import org.joda.time.DateTime;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import es.redmic.models.es.common.deserializer.CustomDateTimeDeserializer;
+import es.redmic.models.es.common.serializer.CustomDateTimeSerializer;
+
+public abstract class BaseUpdatableES extends BaseAbstractStringES {
+
+	@JsonSerialize(using = CustomDateTimeSerializer.class)
+	@JsonDeserialize(using = CustomDateTimeDeserializer.class)
+	private DateTime inserted;
+
+	@JsonSerialize(using = CustomDateTimeSerializer.class)
+	@JsonDeserialize(using = CustomDateTimeDeserializer.class)
+	private DateTime updated;
+
+	public DateTime getInserted() {
+		return inserted;
+	}
+
+	public void setInserted(DateTime inserted) {
+		this.inserted = inserted;
+	}
+
+	public DateTime getUpdated() {
+		return updated;
+	}
+
+	public void setUpdated(DateTime updated) {
+		this.updated = updated;
+	}
+}
