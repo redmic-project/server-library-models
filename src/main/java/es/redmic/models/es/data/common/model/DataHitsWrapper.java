eeee542fc342030b2353f05e07f26e0604d9fdd4
Noel <noelalonso1986@gmail.com>
2021-05-05 14:10:58 +0100

    Añade control de nulos

--- src/main/java/es/redmic/models/es/data/common/model/DataHitsWrapper.java
+++ src/main/java/es/redmic/models/es/data/common/model/DataHitsWrapper.java
@@ -9,9 +9,9 @@ package es.redmic.models.es.data.common.model;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,14 +29,15 @@ import es.redmic.models.es.common.model.BaseES;
 import es.redmic.models.es.common.model.HitsWrapper;
 
 public class DataHitsWrapper<TModel extends BaseES<?>> extends HitsWrapper {
-	
+
 	public DataHitsWrapper() {
+		setHits(new ArrayList<>());
 	}
-	
+
 	public DataHitsWrapper(List<DataHitWrapper<TModel>> hits) {
 		setHits(hits);
 	}
-	
+
 	private List<DataHitWrapper<TModel>> hits;
 
 	public List<DataHitWrapper<TModel>> getHits() {
@@ -46,15 +47,15 @@ public class DataHitsWrapper<TModel extends BaseES<?>> extends HitsWrapper {
 	public void setHits(List<DataHitWrapper<TModel>> hits) {
 		this.hits = hits;
 	}
-	
+
 	@JsonIgnore
 	public List<TModel> getSourceList() {
-		
+
 		List<TModel> sources = new ArrayList<TModel>();
-		
+
 		if (hits == null)
 			return sources;
-		
+
 		int total = hits.size();
 		for(int i=0; i<total; i++) {
 			if (hits.get(i) != null)
--- src/main/java/es/redmic/models/es/data/common/model/DataSearchWrapper.java
+++ src/main/java/es/redmic/models/es/data/common/model/DataSearchWrapper.java
@@ -9,9 +9,9 @@ package es.redmic.models.es.data.common.model;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,8 +28,8 @@ import es.redmic.models.es.common.model.BaseES;
 import es.redmic.models.es.common.model.SearchWrapper;
 
 public class DataSearchWrapper<TModel extends BaseES<?>> extends SearchWrapper {
-	
-	private DataHitsWrapper<TModel> hits;
+
+	private DataHitsWrapper<TModel> hits = new DataHitsWrapper<>();
 
 	public DataHitsWrapper<TModel> getHits() {
 		return hits;
@@ -38,7 +38,7 @@ public class DataSearchWrapper<TModel extends BaseES<?>> extends SearchWrapper {
 	public void setHits(DataHitsWrapper<TModel> hits) {
 		this.hits = hits;
 	}
-	
+
 	@JsonIgnore
 	public Integer getTotal() {
 		return getHits().getTotal();
@@ -48,18 +48,18 @@ public class DataSearchWrapper<TModel extends BaseES<?>> extends SearchWrapper {
 	public List<TModel> getSourceList() {
 		return hits.getSourceList();
 	}
-	
+
 	@JsonIgnore
 	public TModel getSource(int index) {
-		
+
 		if (hits.getTotal() < 1)
 			return null;
 		return (TModel) hits.getHits().get(index).get_source();
 	}
-	
+
 	@JsonIgnore
 	public void setSource(int index, TModel source) {
-		
+
 		if (hits.getTotal() > index)
 			hits.getHits().get(index).set_source(source);
 	}
