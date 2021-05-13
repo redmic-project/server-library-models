7337ee1d94e5e113c900ec6e2392785fa38af574
Noel <noelalonso1986@gmail.com>
2021-05-13 07:33:34 +0100

    Modifica dto de consultas por tipo de dato

--- src/main/java/es/redmic/models/es/common/query/dto/DataQueryDTO.java
+++ src/main/java/es/redmic/models/es/common/query/dto/DataQueryDTO.java
@@ -9,9 +9,9 @@ package es.redmic.models.es.common.query.dto;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,72 +20,42 @@ package es.redmic.models.es.common.query.dto;
  * #L%
  */
 
-import java.util.List;
-
 import javax.validation.Valid;
-import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonView;
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaDescription;
 import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaNotNull;
 
 import es.redmic.models.es.common.view.JsonViewsForQueryDTO;
 
 @JsonFilter("DataQueryDTO")
 @JsonSchemaNotNull
-public class DataQueryDTO extends GeoDataQueryDTO {
+public class DataQueryDTO extends DataAccessibilityQueryDTO {
 
 	public DataQueryDTO() {
 		super();
 	}
 
-	// Data
 	@JsonView(value = { JsonViewsForQueryDTO.Guest.class })
-	@Size(max = 11)
-	private List<String> qFlags;
-
-	@JsonView(value = { JsonViewsForQueryDTO.Guest.class })
-	@Size(max = 5)
-	private List<String> vFlags;
+	@JsonSchemaDescription("Obtiene registros de geometrías comprendidas dentro de los límites especificados.")
+	@Valid
+	private BboxQueryDTO bbox;
 
 	@JsonView(value = { JsonViewsForQueryDTO.Guest.class })
 	@Valid
 	private DateLimitsDTO dateLimits;
 
-	@JsonView(value = { JsonViewsForQueryDTO.Guest.class })
-	private String interval;
-
-	@JsonView(value = { JsonViewsForQueryDTO.Guest.class })
-	@Valid
-	private ZRangeDTO z;
-
-	@JsonView(value = { JsonViewsForQueryDTO.Guest.class })
-	@Valid
-	private List<ValueQueryDTO> value;
-
 	public Boolean queryIsEmpty() {
 		return ((getBbox() == null) && (getPostFilter() == null) && (getText() == null));
 	}
 
-	@JsonProperty("qFlags")
-	public List<String> getQFlags() {
-		return qFlags;
+	public BboxQueryDTO getBbox() {
+		return bbox;
 	}
 
-	@JsonProperty("qFlags")
-	public void setQFlags(List<String> qFlags) {
-		this.qFlags = qFlags;
-	}
-
-	@JsonProperty("vFlags")
-	public List<String> getVFlags() {
-		return vFlags;
-	}
-
-	@JsonProperty("vFlags")
-	public void setVFlags(List<String> vFlags) {
-		this.vFlags = vFlags;
+	public void setBbox(BboxQueryDTO bbox) {
+		this.bbox = bbox;
 	}
 
 	public DateLimitsDTO getDateLimits() {
@@ -94,29 +64,5 @@ public class DataQueryDTO extends GeoDataQueryDTO {
 
 	public void setDateLimits(DateLimitsDTO dateLimits) {
 		this.dateLimits = dateLimits;
-	}
-
-	public String getInterval() {
-		return interval;
-	}
-
-	public void setInterval(String interval) {
-		this.interval = interval;
-	}
-
-	public ZRangeDTO getZ() {
-		return z;
-	}
-
-	public void setZ(ZRangeDTO z) {
-		this.z = z;
-	}
-
-	public List<ValueQueryDTO> getValue() {
-		return value;
-	}
-
-	public void setValue(List<ValueQueryDTO> value) {
-		this.value = value;
 	}
 }
